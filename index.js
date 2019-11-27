e377c3cad909fbc2848165b040a5c393a03644d9
Ana-stasi <anastasia.lapa@gmail.com>
2019-11-27 20:32:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const hostname = '0.0.0.0';
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello there!\n');
+    res.end('Hello world!\n');
 });
 server.listen(process.env.PORT, hostname, () => {
     console.log(`Server running at http://${hostname}:${process.env.PORT}/`);
